4046a526863e7fcc00d0c5672e2bd446bdecde46
Jeff Hokit <jhokit@rocketcom.com>
2022-07-18 15:13:34 -0700

    Feat: use color typealias for simplification
    use typealias for simplification

--- Sources/AstroSwiftCore/AstroCoreColors.swift
+++ Sources/AstroSwiftCore/AstroCoreColors.swift
@@ -17,6 +17,12 @@ import WatchKit
 import AppKit
 #endif
 
+#if canImport(UIKit)
+public typealias NativeColor = UIColor
+#elseif canImport(AppKit)
+public typealias NativeColor = NSColor
+#endif
+
 import SwiftUI
 
 // Extend Color, for Swift and SwiftUI, to include many convenience methods to access core Astro colors.
@@ -30,7 +36,8 @@ public extension Color
     //——————————————————————————————————————————————————————————————————————————————
     private static func astroCoreColor(_ named:String)->Color
     {
-        // The Color constructor is non-failable, so no debug color
+        // The Color constructor is non-failable, so no debug color.
+        // If the color fails to load it is set to white, without an error
         return Color(named, bundle: .module)
     }
     
@@ -172,23 +179,22 @@ public extension Color
 // It's unlikely that an application will need to reference these colors directly, but instead should use the semantic UI colors defined in the AstroSwiftFoundation package.
 // Astro color guidelines - https://www.astrouxds.com/design-guidelines/color
 //
-#if os(iOS) || os(tvOS) || os(watchOS)
-public extension UIColor
+public extension NativeColor
 {
     // If an Astro color fails to load from resources, show this noticeable yellow debug color instead
-    private static var astroCoreDebugColor = UIColor.yellow
+    private static var astroCoreDebugColor = NativeColor.yellow
 
     //——————————————————————————————————————————————————————————————————————————————
-    // Wrap the the UIColor constructor to return a debug color instead of nil on failure
+    // Wrap the the NativeColor constructor to return a debug color instead of nil on failure
     //——————————————————————————————————————————————————————————————————————————————
-    private static func astroCoreColor(named:String)->UIColor
+    private static func astroCoreColor(named:String)->NativeColor
     {
         #if os(iOS) || os(tvOS)
-        return UIColor(named:named, in: .module, compatibleWith: nil) ?? astroCoreDebugColor
+        return NativeColor(named:named, in: .module, compatibleWith: nil) ?? astroCoreDebugColor
         #endif
         
-        #if os(watchOS)
-        return UIColor(named:named) ?? astroCoreDebugColor
+        #if os(watchOS) || os(macOS)
+        return NativeColor(named:named) ?? astroCoreDebugColor
         #endif
     }
     
@@ -197,247 +203,115 @@ public extension UIColor
     //——————————————————————————————————————————————————————————————————————————————
 
     //MARK: astroUIPrimary
-    static var astroUIPrimary:UIColor
+    static var astroUIPrimary:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Color")}
     
-    static var astroUIPrimaryDarken1:UIColor
+    static var astroUIPrimaryDarken1:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Darken 1 Color")}
 
-    static var astroUIPrimaryDarken2:UIColor
+    static var astroUIPrimaryDarken2:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Darken 2 Color")}
 
-    static var astroUIPrimaryDarken3:UIColor
+    static var astroUIPrimaryDarken3:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Darken 3 Color")}
 
-    static var astroUIPrimaryDarken4:UIColor
+    static var astroUIPrimaryDarken4:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Darken 4 Color")}
     
-    static var astroUIPrimaryLighten1:UIColor
+    static var astroUIPrimaryLighten1:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Lighten 1 Color")}
 
-    static var astroUIPrimaryLighten2:UIColor
+    static var astroUIPrimaryLighten2:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Lighten 2 Color")}
 
-    static var astroUIPrimaryLighten3:UIColor
+    static var astroUIPrimaryLighten3:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Lighten 3 Color")}
 
-    static var astroUIPrimaryLighten4:UIColor
+    static var astroUIPrimaryLighten4:NativeColor
     {return astroCoreColor(named:"Astro UI Primary Lighten 4 Color")}
 
     //MARK: astroUISecondary
-    static var astroUISecondary:UIColor
+    static var astroUISecondary:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Color")}
 
-    static var astroUISecondaryDarken1:UIColor
+    static var astroUISecondaryDarken1:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Darken 1 Color")}
 
-    static var astroUISecondaryDarken2:UIColor
+    static var astroUISecondaryDarken2:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Darken 2 Color")}
 
-    static var astroUISecondaryDarken3:UIColor
+    static var astroUISecondaryDarken3:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Darken 3 Color")}
 
-    static var astroUISecondaryDarken4:UIColor
+    static var astroUISecondaryDarken4:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Darken 4 Color")}
     
-    static var astroUISecondaryLighten1:UIColor
+    static var astroUISecondaryLighten1:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Lighten 1 Color")}
 
-    static var astroUISecondaryLighten2:UIColor
+    static var astroUISecondaryLighten2:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Lighten 2 Color")}
 
-    static var astroUISecondaryLighten3:UIColor
+    static var astroUISecondaryLighten3:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Lighten 3 Color")}
 
-    static var astroUISecondaryLighten4:UIColor
+    static var astroUISecondaryLighten4:NativeColor
     {return astroCoreColor(named:"Astro UI Secondary Lighten 4 Color")}
 
     //MARK: astroUITertiary
-    static var astroUITertiary:UIColor
+    static var astroUITertiary:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Color")}
 
-    static var astroUITertiaryDarken1:UIColor
+    static var astroUITertiaryDarken1:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Darken 1 Color")}
 
-    static var astroUITertiaryDarken2:UIColor
+    static var astroUITertiaryDarken2:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Darken 2 Color")}
 
-    static var astroUITertiaryDarken3:UIColor
+    static var astroUITertiaryDarken3:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Darken 3 Color")}
 
-    static var astroUITertiaryDarken4:UIColor
+    static var astroUITertiaryDarken4:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Darken 4 Color")}
     
-    static var astroUITertiaryLighten1:UIColor
+    static var astroUITertiaryLighten1:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Lighten 1 Color")}
 
-    static var astroUITertiaryLighten2:UIColor
+    static var astroUITertiaryLighten2:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Lighten 2 Color")}
 
-    static var astroUITertiaryLighten3:UIColor
+    static var astroUITertiaryLighten3:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Lighten 3 Color")}
 
-    static var astroUITertiaryLighten4:UIColor
+    static var astroUITertiaryLighten4:NativeColor
     {return astroCoreColor(named:"Astro UI Tertiary Lighten 4 Color")}
 
     //MARK: astroUIQuaternary
-    static var astroUIQuaternary:UIColor
+    static var astroUIQuaternary:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Color")}
     
-    static var astroUIQuaternaryDarken1:UIColor
+    static var astroUIQuaternaryDarken1:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Darken 1 Color")}
 
-    static var astroUIQuaternaryDarken2:UIColor
+    static var astroUIQuaternaryDarken2:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Darken 2 Color")}
 
-    static var astroUIQuaternaryDarken3:UIColor
+    static var astroUIQuaternaryDarken3:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Darken 3 Color")}
 
-    static var astroUIQuaternaryDarken4:UIColor
+    static var astroUIQuaternaryDarken4:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Darken 4 Color")}
     
-    static var astroUIQuaternaryLighten1:UIColor
+    static var astroUIQuaternaryLighten1:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Lighten 1 Color")}
 
-    static var astroUIQuaternaryLighten2:UIColor
+    static var astroUIQuaternaryLighten2:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Lighten 2 Color")}
 
-    static var astroUIQuaternaryLighten3:UIColor
+    static var astroUIQuaternaryLighten3:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Lighten 3 Color")}
 
-    static var astroUIQuaternaryLighten4:UIColor
+    static var astroUIQuaternaryLighten4:NativeColor
     {return astroCoreColor(named:"Astro UI Quaternary Lighten 4 Color")}
 }
-#endif
 
-#if os(macOS)
-public extension NSColor
-{
-    // If an Astro color fails to load from resources, show this noticeable yellow debug color instead
-    private static var astroCoreDebugColor = NSColor.yellow
-
-    //——————————————————————————————————————————————————————————————————————————————
-    // Wrap the the NSColor constructor to return a debug color instead of nil on failure
-    //——————————————————————————————————————————————————————————————————————————————
-    private static func astroCoreColor(named:String)->NSColor
-    {
-        return NSColor(named:named) ?? astroCoreDebugColor
-    }
-    
-    //——————————————————————————————————————————————————————————————————————————————
-    // Astro Core UI colors
-    //——————————————————————————————————————————————————————————————————————————————
-
-    //MARK: astroUIPrimary
-    static var astroUIPrimary:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Color")}
-    
-    static var astroUIPrimaryDarken1:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Darken 1 Color")}
-
-    static var astroUIPrimaryDarken2:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Darken 2 Color")}
-
-    static var astroUIPrimaryDarken3:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Darken 3 Color")}
-
-    static var astroUIPrimaryDarken4:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Darken 4 Color")}
-    
-    static var astroUIPrimaryLighten1:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Lighten 1 Color")}
-
-    static var astroUIPrimaryLighten2:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Lighten 2 Color")}
-
-    static var astroUIPrimaryLighten3:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Lighten 3 Color")}
-
-    static var astroUIPrimaryLighten4:NSColor
-    {return astroCoreColor(named:"Astro UI Primary Lighten 4 Color")}
-
-    //MARK: astroUISecondary
-    static var astroUISecondary:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Color")}
-
-    static var astroUISecondaryDarken1:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Darken 1 Color")}
-
-    static var astroUISecondaryDarken2:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Darken 2 Color")}
-
-    static var astroUISecondaryDarken3:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Darken 3 Color")}
-
-    static var astroUISecondaryDarken4:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Darken 4 Color")}
-    
-    static var astroUISecondaryLighten1:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Lighten 1 Color")}
-
-    static var astroUISecondaryLighten2:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Lighten 2 Color")}
-
-    static var astroUISecondaryLighten3:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Lighten 3 Color")}
-
-    static var astroUISecondaryLighten4:NSColor
-    {return astroCoreColor(named:"Astro UI Secondary Lighten 4 Color")}
-
-    //MARK: astroUITertiary
-    static var astroUITertiary:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Color")}
-
-    static var astroUITertiaryDarken1:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Darken 1 Color")}
-
-    static var astroUITertiaryDarken2:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Darken 2 Color")}
-
-    static var astroUITertiaryDarken3:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Darken 3 Color")}
-
-    static var astroUITertiaryDarken4:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Darken 4 Color")}
-    
-    static var astroUITertiaryLighten1:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Lighten 1 Color")}
-
-    static var astroUITertiaryLighten2:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Lighten 2 Color")}
-
-    static var astroUITertiaryLighten3:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Lighten 3 Color")}
-
-    static var astroUITertiaryLighten4:NSColor
-    {return astroCoreColor(named:"Astro UI Tertiary Lighten 4 Color")}
-
-    //MARK: astroUIQuaternary
-    static var astroUIQuaternary:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Color")}
-    
-    static var astroUIQuaternaryDarken1:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Darken 1 Color")}
-
-    static var astroUIQuaternaryDarken2:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Darken 2 Color")}
-
-    static var astroUIQuaternaryDarken3:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Darken 3 Color")}
-
-    static var astroUIQuaternaryDarken4:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Darken 4 Color")}
-    
-    static var astroUIQuaternaryLighten1:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Lighten 1 Color")}
-
-    static var astroUIQuaternaryLighten2:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Lighten 2 Color")}
-
-    static var astroUIQuaternaryLighten3:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Lighten 3 Color")}
-
-    static var astroUIQuaternaryLighten4:NSColor
-    {return astroCoreColor(named:"Astro UI Quaternary Lighten 4 Color")}
-}
-#endif
